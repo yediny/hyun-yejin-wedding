b4c071f9cadabb3c46f935ee0e603711542a21b1
Yejin <yejin@Yejinui-noteubug.local>
2026-02-16 22:26:55 +0900

    gift

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import Calendar from '../components/calendar.svelte';
 	import Letter from '../components/letter.svelte';
-	import Rsvp from '../components/rsvp.svelte';
 	import Cover from '../components/cover.svelte';
 	import Location from '../components/location.svelte';
 	import type { PageProps } from './$types';
 	import Gallery from '../components/gallery.svelte';
-
+	import GiftInfo from '../components/gift-info.svelte';
 	let { form }: PageProps = $props();
 </script>
 
@@ -14,4 +13,5 @@
 <Letter />
 <Gallery />
 <Calendar />
+<GiftInfo />
 <Location />
